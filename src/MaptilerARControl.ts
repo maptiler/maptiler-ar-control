5c651be2c78f8a41ab05ad9fca48abd74ae01faf
Kate Miháliková <kate@katemihalikova.cz>
2025-12-18 11:45:37 +0100

    RD-1519 Fix tiles calculations (#30)
    * RD-1519 Fix tiles calculations
    
    * RD-1519 Limit tile index calculation to allowed latitudes only
    
    * RD-1519 Fix variable

--- src/MaptilerARControl.ts
+++ src/MaptilerARControl.ts
@@ -46,9 +46,11 @@ function removeDomNode(node: HTMLElement) {
   node.parentNode.removeChild(node);
 }
 
-const MIN_TERRAIN_ZOOM = 12;
+const MIN_TERRAIN_ZOOM = 0;
+const MAX_TERRAIN_ZOOM = 12;
 const TERRAIN_TILE_SIZE = 512;
 const MAX_ZOOM = 16;
+const MAX_VALID_LATITUDE = 85.0511287;
 
 export function mapTextureDataToCanvas(mtd: MapTextureData): HTMLCanvasElement {
   // Create a blank canvas
@@ -71,9 +73,15 @@ async function createMosaic(
   tileIndexTopLeft: TileIndex2D,
   tileIndexBottomRight: TileIndex2D,
   zoom: number,
+  wrapsOverAntimeridian: boolean,
   tilesetURLs: Array<string>
 ): Promise<HTMLCanvasElement> {
-  const nbTileX = tileIndexBottomRight.x - tileIndexTopLeft.x + 1;
+  const numberOfTiles = 2 ** zoom;
+  const nbTileX =
+    tileIndexBottomRight.x -
+    tileIndexTopLeft.x +
+    1 +
+    (wrapsOverAntimeridian ? numberOfTiles : 0);
   const nbTileY = tileIndexBottomRight.y - tileIndexTopLeft.y + 1;
   const canvas = document.createElement("canvas");
   canvas.width = nbTileX * TERRAIN_TILE_SIZE;
@@ -87,21 +95,29 @@ async function createMosaic(
 
   const promises = [];
 
-  for (let j = tileIndexTopLeft.y; j <= tileIndexBottomRight.y; j += 1) {
-    for (let i = tileIndexTopLeft.x; i <= tileIndexBottomRight.x; i += 1) {
-      const imageUrlPattern =
-        tilesetURLs[~~(Math.random() * tilesetURLs.length)];
-      const imageUrl = imageUrlPattern
-        .replace("{x}", i.toString())
-        .replace("{y}", j.toString())
-        .replace("{z}", zoom.toString());
-      promises.push(
-        injectToContext(imageUrl, context, [
-          shiftX * TERRAIN_TILE_SIZE,
-          shiftY * TERRAIN_TILE_SIZE,
-        ])
-      );
-      shiftX++;
+  const prepareTile = (x: number, y: number) => {
+    const imageUrlPattern = tilesetURLs[~~(Math.random() * tilesetURLs.length)];
+    const imageUrl = imageUrlPattern
+      .replace("{x}", x.toString())
+      .replace("{y}", y.toString())
+      .replace("{z}", zoom.toString());
+    promises.push(
+      injectToContext(imageUrl, context, [
+        shiftX * TERRAIN_TILE_SIZE,
+        shiftY * TERRAIN_TILE_SIZE,
+      ])
+    );
+    shiftX++;
+  };
+
+  for (let y = tileIndexTopLeft.y; y <= tileIndexBottomRight.y; y += 1) {
+    if (wrapsOverAntimeridian) {
+      for (let x = tileIndexTopLeft.x; x < numberOfTiles; x += 1)
+        prepareTile(x, y);
+      for (let x = 0; x <= tileIndexBottomRight.x; x += 1) prepareTile(x, y);
+    } else {
+      for (let x = tileIndexTopLeft.x; x <= tileIndexBottomRight.x; x += 1)
+        prepareTile(x, y);
     }
     shiftX = 0;
     shiftY++;
@@ -696,11 +712,21 @@ export class MaptilerARControl extends EventEmitter implements IControl {
   private async computeTerrainData() {
     this.emit("startComputeTerrainData", {});
 
-    // For fetching tiles, the zoom level we use must be integer and not above the max zoom level for terrain data
-    const zoom = Math.min(Math.floor(this.map.getZoom()), MIN_TERRAIN_ZOOM);
+    // For fetching tiles, the zoom level we use must be non-negative integer and not above the max zoom level for terrain data
+    const zoom = Math.max(
+      Math.min(Math.floor(this.map.getZoom()), MAX_TERRAIN_ZOOM),
+      MIN_TERRAIN_ZOOM
+    );
+    const numberOfTiles = 2 ** zoom;
     const bounds = this.map.getBounds();
-    const north = bounds.getNorth();
-    const south = bounds.getSouth();
+    const north = Math.max(
+      Math.min(bounds.getNorth(), MAX_VALID_LATITUDE),
+      -MAX_VALID_LATITUDE
+    );
+    const south = Math.max(
+      Math.min(bounds.getSouth(), MAX_VALID_LATITUDE),
+      -MAX_VALID_LATITUDE
+    );
     const east = bounds.getEast();
     const west = bounds.getWest();
 
@@ -710,7 +736,7 @@ export class MaptilerARControl extends EventEmitter implements IControl {
       false
     );
     const tileIndexTopLeft = {
-      x: tileIndexTopLeftArr[0],
+      x: west === -180 ? 0 : tileIndexTopLeftArr[0],
       y: tileIndexTopLeftArr[1],
     } as TileIndex2D;
 
@@ -728,18 +754,21 @@ export class MaptilerARControl extends EventEmitter implements IControl {
       x: tileIndexBottomRightArr[0],
       y: tileIndexBottomRightArr[1],
     } as TileIndex2D;
-    const tileIndexBottomRightFloored = {
-      x: Math.floor(tileIndexBottomRight.x),
-      y: Math.floor(tileIndexBottomRight.y),
+    const tileIndexBottomRightCeiled = {
+      x: Math.ceil(tileIndexBottomRight.x) - 1,
+      y: Math.ceil(tileIndexBottomRight.y) - 1,
     };
+
+    const wrapsOverAntimeridian = tileIndexBottomRight.x < tileIndexTopLeft.x;
 
     // The terrainCanvas is rounded up to be aligned with the tile
     const sdkConfig = this.map.getSdkConfig();
     const mtsid = this.map.getMaptilerSessionId();
     const terrainCanvas = await createMosaic(
       tileIndexTopLeftFloored,
-      tileIndexBottomRightFloored,
+      tileIndexBottomRightCeiled,
       zoom,
+      wrapsOverAntimeridian,
       [
         `https://api.maptiler.com/tiles/terrain-rgb-v2/{z}/{x}/{y}.webp?key=${sdkConfig.apiKey}&mtsid=${mtsid}&module=xr`,
       ]
@@ -757,7 +786,10 @@ export class MaptilerARControl extends EventEmitter implements IControl {
 
     const size = [
       Math.ceil(
-        TERRAIN_TILE_SIZE * (tileIndexBottomRight.x - tileIndexTopLeft.x)
+        TERRAIN_TILE_SIZE *
+          (tileIndexBottomRight.x -
+            tileIndexTopLeft.x +
+            (wrapsOverAntimeridian ? numberOfTiles : 0))
       ),
       Math.ceil(
         TERRAIN_TILE_SIZE * (tileIndexBottomRight.y - tileIndexTopLeft.y)
